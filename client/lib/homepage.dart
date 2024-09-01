850cc7f2aa547a8fcecb4bcd4ba21e7ae3700635
Mattia Cincotta <mattia.cinco@gmail.com>
2024-09-01 17:31:57 +0200

    added main.dart and homepage.dart

---
+++ client/lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:client/main.dart';
+import 'package:flutter/material.dart';
+
+
+class CinemaAppHomepage extends StatefulWidget {
+  const CinemaAppHomepage({super.key});
+
+  @override
+  State<CinemaAppHomepage> createState() => _CinemaAppHomepageState();
+}
+
+class _CinemaAppHomepageState extends State<CinemaAppHomepage> {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold();
+  }
+}
+
+
+
