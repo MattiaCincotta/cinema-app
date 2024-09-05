e7efa3df170205a33d5f813391d47f1e2ce68e12
Mattia Cincotta <mattia.cinco@gmail.com>
2024-09-05 18:11:00 +0200

    fixed graphics

--- client/lib/favoritefilm.dart
+++ client/lib/favoritefilm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:collection';
 
 class FavoriteFilmPage extends StatefulWidget {
   const FavoriteFilmPage({super.key});
@@ -8,89 +9,9 @@ class FavoriteFilmPage extends StatefulWidget {
 }
 
 class _FavoriteFilmPageState extends State<FavoriteFilmPage> {
-  int _favoriteCount = 0; 
-
-  Widget createImageWithIcon(String imageUrl, String filmName) {
-    bool isFavorite = false; 
-
-    return StatefulBuilder(
-      builder: (BuildContext context, StateSetter setState) {
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          elevation: 5, 
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  filmName,
-                  style: const TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.redAccent,
-                  ),
-                  maxLines: 2, 
-                  overflow: TextOverflow
-                      .ellipsis,
-                ),
-                const SizedBox(height: 15),
-                Image.network(
-                  imageUrl,
-                  width: double.infinity,
-                  height: 275,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    const Spacer(),
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          isFavorite = !isFavorite;
-                          if (isFavorite) {
-                            _favoriteCount++;
-                          } else {
-                            _favoriteCount--;
-                          }
-                        });
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                          boxShadow: isFavorite
-                              ? [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.3),
-                                    blurRadius: 10,
-                                    offset: const Offset(0, 4),
-                                  ),
-                                ]
-                              : [],
-                        ),
-                        child: Icon(
-                          isFavorite ? Icons.favorite_border : Icons.favorite,
-                          color: isFavorite ? Colors.grey: Colors.redAccent,
-                          size: 45, 
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
+  HashSet<int> numeri = HashSet<int>(); 
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,9 +22,12 @@ class _FavoriteFilmPageState extends State<FavoriteFilmPage> {
             color: Colors.white,
             size: 28,
           ),
-          onPressed: () {
-            Navigator.pop(
-                context); 
+          onPressed: () async {
+            bool shouldExit = await _showExitConfirmationDialog();
+            if (shouldExit) {
+              // ignore: use_build_context_synchronously
+              Navigator.pop(context);
+            }
           },
         ),
         title: const Text(
@@ -137,7 +61,7 @@ class _FavoriteFilmPageState extends State<FavoriteFilmPage> {
                     borderRadius: BorderRadius.circular(15),
                   ),
                   child: Text(
-                    '$_favoriteCount',
+                    numeri.length.toString(),
                     style: const TextStyle(
                       color: Colors.white,
                       fontSize: 20,
@@ -174,7 +98,7 @@ class _FavoriteFilmPageState extends State<FavoriteFilmPage> {
             children: [
               const SizedBox(height: 50),
               createImageWithIcon(
-                'https://www.agireora.org/img/news/pollo-bianco-primo-piano.jpg', 
+                'https://www.agireora.org/img/news/pollo-bianco-primo-piano.jpg',
                 'La storia della principessa splendente e delle sue avventure straordinarie',
               ),
               createImageWithIcon(
@@ -182,7 +106,7 @@ class _FavoriteFilmPageState extends State<FavoriteFilmPage> {
                 'Un altro film con un titolo molto lungo che si estende per diverse righe',
               ),
               createImageWithIcon(
-                'https://www.agireora.org/img/news/pollo-bianco-primo-piano.jpg', 
+                'https://www.agireora.org/img/news/pollo-bianco-primo-piano.jpg',
                 'La storia della principessa splendente',
               ),
             ],
@@ -190,5 +114,115 @@ class _FavoriteFilmPageState extends State<FavoriteFilmPage> {
         ),
       ),
     );
+  }
+
+  Widget createImageWithIcon(String imageUrl, String filmName) {
+    bool isFavorite = false;
+
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  filmName,
+                  style: const TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 15),
+                Image.network(
+                  imageUrl,
+                  width: double.infinity,
+                  height: 275,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    const Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isFavorite = !isFavorite;
+                          if (isFavorite) {
+                            //_favoriteCount++;
+                          } else {
+                            //_favoriteCount--;
+                          }
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          boxShadow: isFavorite
+                              ? [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.3),
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 4),
+                                  ),
+                                ]
+                              : [],
+                        ),
+                        child: Icon(
+                          isFavorite ? Icons.favorite_border : Icons.favorite,
+                          color: isFavorite ? Colors.grey : Colors.redAccent,
+                          size: 45,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  // Funzione per mostrare il popup di conferma
+  Future<bool> _showExitConfirmationDialog() async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Conferma'),
+              content: const Text(
+                  'Vuoi eliminare i film dai preferiti prima di uscire?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false); // Resta nella pagina
+                  },
+                  child: const Text('No'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(true); // Conferma ed esce dalla pagina
+                  },
+                  child: const Text('Sì'),
+                ),
+              ],
+            );
+          },
+        ) ??
+        false; // Torna a false se il dialogo viene chiuso senza selezionare nulla
   }
 }
