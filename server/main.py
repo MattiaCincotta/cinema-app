be0972e6f9cc14bc59dc273f65958c4957a779b8
Mattia Cincotta <mattia.cinco@gmail.com>
2024-09-21 23:32:27 +0200

    fixed bug

--- server/main.py
+++ server/main.py
@@ -246,7 +246,7 @@ def add_director():
         return jsonify("invalid token"), 401
     
     data = request.get_json()
-    if data and isinstance(data['name'], str) and isinstance(data['biography'], str) and isinstance(data['image_url'], str) and isinstance(data['categories_id'], list[str]):
+    if data and isinstance(data['name'], str) and isinstance(data['biography'], str) and isinstance(data['image_url'], str) and isinstance(data['categories_id'], list):
         success = DirectorManager.add_director(Director(0, data['name'], data['image_url']), data['biography'], data['categories_id'])
         if success:
             return jsonify("director added"), 200
